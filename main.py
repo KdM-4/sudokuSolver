a796315da592b9b92bbcdc81ca7608e727703cfb
Korbin de Man <korbindeman@hotmail.com>
2021-03-05 20:54:03 -0400

    refactored to make it not stupid

--- main.py
+++ main.py
@@ -2,54 +2,78 @@ import math
 
 
 class Cell:
-    def __init__(self, block, row, col, value, possibleValues) -> None:
+    def __init__(self, block, row, col, value) -> None:
         self.block = block
         self.row = row
         self.col = col
         self.value = value
-        self.possibleValues = possibleValues
 
 
 class Board:
-    def __init__(self, board) -> None:
-        self.list = board
-        self.update()
+    cells = []
+    blocks = [[] for i in range(9)]
+    rows = [[] for i in range(9)]
+    cols = [[] for i in range(9)]
 
-    def update(self):
-        """STUPID FUNCTION THAT DOES STUPID STUFF AAH"""
-        # replace this garbage later
-        # please
-        # STUPID ASS IDEA!!!
-        self.blocks = [[] for i in range(9)]
-        self.rows = [[] for i in range(9)]
-        self.cols = [[] for i in range(9)]
-        for i in range(81):
-            value = self.list[i].value
-            block = math.floor(i / 3) % 3 + math.floor(i / 27) * 3
-            row = math.floor(i / 9)
-            col = i % 9
-            self.blocks[block].append(value)
-            self.rows[row].append(value)
-            self.cols[col].append(value)
+    def __init__(self, boardId) -> None:
+        for cellIndex in range(81):
+            # Get corresponding indexes for positional lists
+            block = math.floor(cellIndex / 3) % 3 + math.floor(cellIndex / 27) * 3
+            row = math.floor(cellIndex / 9)
+            col = cellIndex % 9
+            # Create cell object
+            cell = Cell(block, row, col, int(boardId[cellIndex]))
+            # Add cell to lists
+            self.cells.append(cell)
+            self.blocks[block].append(cell.value)
+            self.rows[row].append(cell.value)
+            self.cols[col].append(cell.value)
 
-    def getCode(self) -> "code":
-        """Returns the code of the current state of the board"""
+    def updateCellValue(self, index, value) -> None:
+        cell = self.cells[index]
+        cell.value = value
+        blockIndex = cell.row % 3 * 3 + cell.col % 3
+        self.blocks[cell.block][blockIndex] = value
+        self.rows[cell.row][cell.col] = value
+        self.cols[cell.col][cell.row] = value
+
+    def checkIfFilled(self):
+        for cell in self.cells:
+            if cell.value == 0:
+                return False
+        return True
+
+    def getCode(self):
         code = ""
-        for cell in self.list:
+        for cell in self.cells:
             code += str(cell.value)
         return code
 
 
-def generateBoard(code) -> Board:
-    """Returns a Board object using a sudoku code"""
-    board = []
-    for i in range(81):
-        block = math.floor(i / 3) % 3 + math.floor(i / 27) * 3
-        row = math.floor(i / 9)
-        col = i % 9
-        # print(i, row, col, block)
-        board.append(Cell(block, row, col, int(code[i]), []))
-    return Board(board)
+def findEmptyCell(cells, currentIndex):
+    for index in range(currentIndex + 1, 81):
+        if cells[index].value == 0:
+            return index
+    return -1
+
+
+def getValue(board, index):
+    possibleValues = [num for num in range(1, 10)]
+    cell = board.cells[index]
+    for i in range(9):
+        blockVal = board.blocks[cell.block][i]
+        rowVal = board.rows[cell.row][i]
+        colVal = board.cols[cell.col][i]
+        if blockVal != 0 and blockVal in possibleValues:
+            possibleValues.remove(blockVal)
+        if rowVal != 0 and rowVal in possibleValues:
+            possibleValues.remove(rowVal)
+        if colVal != 0 and colVal in possibleValues:
+            possibleValues.remove(colVal)
+    if len(possibleValues) == 1:
+        return possibleValues[0]
+    else:
+        return 0
 
 
 def createSeperator(start, mid, end) -> str:
@@ -77,66 +101,30 @@ def drawBoard(code) -> None:
         print(rowString)
 
 
-def findEmptyCell(cells, i):
-    for cellI in range(i + 1, 81):
-        if cells[cellI].value == 0:
-            return cells.index(cells[cellI])
-    return -1
-
-
-def getPossibleValues(board, index):
-    possibleValues = [i for i in range(1, 10)]
-    cell = board.list[index]
-    for i in range(9):
-        blockVal = board.blocks[cell.block][i]
-        rowVal = board.rows[cell.row][i]
-        colVal = board.cols[cell.col][i]
-        if blockVal != 0 and blockVal in possibleValues:
-            possibleValues.remove(blockVal)
-        if rowVal != 0 and rowVal in possibleValues:
-            possibleValues.remove(rowVal)
-        if colVal != 0 and colVal in possibleValues:
-            possibleValues.remove(colVal)
-    # print(possibleValues)
-    return possibleValues
-
-
-def solve(code) -> "code":
-    board = generateBoard(code)
-    cells = board.list
-
-    drawBoard(board.getCode())
-
-    i = 0
+def solve(boardId):
+    board = Board(boardId)
 
     solved = False
-    tel = 0
-    _max = 300
-    while solved == False and tel < _max:
-        emptyIndex = findEmptyCell(cells, i)
-        if emptyIndex == i:
-            i = 0
-        else:
-            i = emptyIndex
-            if emptyIndex == -1:
-                # solved = True
-                solved = False
-            cells[emptyIndex].possibleValues = getPossibleValues(board, emptyIndex)
-            if len(cells[emptyIndex].possibleValues) == 1:
-                cells[emptyIndex].value = cells[emptyIndex].possibleValues[0]
-            board.update()
-        tel += 1
 
-    drawBoard(board.getCode())
+    cellIndex = 0
+
+    while solved == False:
+        cellIndex = findEmptyCell(board.cells, cellIndex)
+        if cellIndex != -1:
+            cellValue = getValue(board, cellIndex)
+            board.updateCellValue(cellIndex, cellValue)
+        elif board.checkIfFilled():
+            solved = True
+        else:
+            cellIndex = 0
 
     return board.getCode()
 
 
-code = (
+boardId = (
     "530070000600195000098000060800060003400803001700020006060000280000419005000080079"
-    # "100470000000162704060000000871045906300000051256090070027010508015680042603000100"
-    # "000240003000000029706000000000000062100000000564000000400008790001004800090013000"
-    # "600000300900470100700950000000390000030000002020000804006201000000000645408000000"
 )
 
-solve(code)
+drawBoard(boardId)
+solvedId = solve(boardId)
+drawBoard(solvedId)
