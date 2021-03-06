50b2356fb97e47f6223cb2b8321dd9fc56c03159
Korbin de Man <korbindeman@hotmail.com>
2021-03-06 09:22:02 -0400

    it works kinda better but it still gets stuck

--- main.py
+++ main.py
@@ -59,21 +59,49 @@ def findEmptyCell(cells, currentIndex) -> int:
     return -1
 
 
+def findUnique(_list):
+    uniques = []
+    # Insert all list elements in hash table
+    mp = {}
+    for i in range(len(_list)):
+        if _list[i] not in mp:
+            mp[_list[i]] = 0
+        mp[_list[i]] += 1
+
+    # Traverse through map only and
+    for x in mp:
+        if mp[x] == 1:
+            uniques.append(x)
+
+    return uniques
+
+
 def getValue(board, index) -> int:
     possibleValues = [num for num in range(1, 10)]
     cell = board.cells[index]
+    blockSuperPossibilities = []
     for i in range(9):
-        block = board.blocks[cell.block][i]
-        row = board.rows[cell.row][i]
-        col = board.cols[cell.col][i]
-        if block.value != 0 and block.value in possibleValues:
-            possibleValues.remove(block.value)
-        if row.value != 0 and row.value in possibleValues:
-            possibleValues.remove(row.value)
-        if col.value != 0 and col.value in possibleValues:
-            possibleValues.remove(col.value)
-    if len(possibleValues) == 1:
-        return possibleValues[0], possibleValues
+        blockCell = board.blocks[cell.block][i]
+        rowCell = board.rows[cell.row][i]
+        colCell = board.cols[cell.col][i]
+        if blockCell.value != 0 and blockCell.value in possibleValues:
+            possibleValues.remove(blockCell.value)
+        if rowCell.value != 0 and rowCell.value in possibleValues:
+            possibleValues.remove(rowCell.value)
+        if colCell.value != 0 and colCell.value in possibleValues:
+            possibleValues.remove(colCell.value)
+        for possibility in blockCell.possibleValues:
+            blockSuperPossibilities.append(possibility)
+    for possibility in possibleValues:
+        blockSuperPossibilities.append(possibility)
+    blockPossiblePossibilities = [
+        x for x in blockSuperPossibilities if x in possibleValues
+    ]
+    uniqueBlockPossibilities = findUnique(blockPossiblePossibilities)
+    if len(uniqueBlockPossibilities) == 1:
+        return uniqueBlockPossibilities[0], []
+    elif len(possibleValues) == 1:
+        return possibleValues[0], []
     else:
         return 0, possibleValues
 
@@ -128,7 +156,8 @@ def solve(boardId) -> str:
 
 
 boardId = (
-    "530070000600195000098000060800060003400803001700020006060000280000419005000080079"
+    # "530070000600195000098000060800060003400803001700020006060000280000419005000080079"
+    "001003000000850000040026800079005002002000000500000004000000027600040000000190680"
 )
 
 drawBoard(boardId)
